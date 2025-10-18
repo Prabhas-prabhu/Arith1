e439e3ec5a98d18f286b94bbb4b4147b72b0031b
Prabhas-prabhu <konchadaprabhas@gmail.com>
2025-10-18 11:08:40 +0530

    Add files via upload

---
+++ ARITH.C
@@ -0,0 +1,14 @@
+	#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	clrscr();
+	int a,b;
+	printf("\n enter any two numbers:");
+	scanf("%d%d",&a,&b);
+	printf("\na+b=%d is the required number",a+b);
+	printf("\na-b=%d is the required number",a-b);
+	printf("\na*b=%d is the required number",a*b);
+	printf("\na/b=%d is the required number",a/b);
+	getch();
+	}
